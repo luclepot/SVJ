8c2fa342aecd6ce05e4243605f3974d0b006dca8
Annapaola de Cosa (UniZ) [decosa] <decosa@t3ui03.psi.ch>
2018-07-03 17:11:58 +0200

    adding function to create ROC curves

--- BDT/TMVAReader.py
+++ BDT/TMVAReader.py
@@ -18,6 +18,8 @@ import ROOT
 import copy
 import commands, os.path
 import numpy as n
+from SVJ.MakePlot.samples.QCD import QCD
+from SVJ.MakePlot.samples.signals import *
 
 ROOT.gROOT.Reset();
 ROOT.gROOT.SetStyle('Plain')
@@ -29,60 +31,83 @@ ROOT.TH1.AddDirectory(False)
 
 
 
-def getBDT(filename):
+def getSingleBDT(filename, label):
 
-    f = ROOT.TFile.Open(filename)
-    t = f.Get("tree")
+      try:
+        f = ROOT.TFile.Open(filename)
+      except IOError:
+        print "Cannot open ", filename
+      else:
+        #print "Opening file ",  filename
+        t = f.Get("tree")
+        reader = ROOT.TMVA.Reader()
+      
+        mult = array.array('f',[0]) 
+        girth = array.array('f',[0]) 
+        tau21 = array.array('f',[0]) 
+        tau32 = array.array('f',[0]) 
+        axisminor = array.array('f',[0]) 
+        deltaphi = array.array('f',[0]) 
+        msd = array.array('f',[0])
+        pt = array.array('f',[0]) 
+      
+        reader.AddVariable("mult", mult)
+        reader.AddVariable("axisminor", axisminor)
+        reader.AddVariable("girth", girth)
+        reader.AddVariable("tau21", tau21)
+        reader.AddVariable("tau32", tau32)
+        reader.AddVariable("msd", msd)
+        reader.AddVariable("deltaphi", deltaphi)
+        reader.AddSpectator("pt", pt)
+        
+        weightsfile = "weights/TMVAClassification_BDTG.weights.xml"
+        reader.BookMVA("BDTG", weightsfile)
+        nentries = t.GetEntries()
     
-    reader = ROOT.TMVA.Reader()
-    
-    mult = array.array('f',[0]) 
-    girth = array.array('f',[0]) 
-    tau21 = array.array('f',[0]) 
-    tau32 = array.array('f',[0]) 
-    axisminor = array.array('f',[0]) 
-    deltaphi = array.array('f',[0]) 
-    msd = array.array('f',[0])
-    pt = array.array('f',[0]) 
-    
-    reader.AddVariable("mult", mult)
-    reader.AddVariable("axisminor", axisminor)
-    reader.AddVariable("girth", girth)
-    reader.AddVariable("tau21", tau21)
-    reader.AddVariable("tau32", tau32)
-    reader.AddVariable("msd", msd)
-    reader.AddVariable("deltaphi", deltaphi)
-    reader.AddSpectator("pt", pt)
+        t.SetBranchAddress("axisminor",axisminor)
+        t.SetBranchAddress("girth",girth)
+        t.SetBranchAddress("tau21",tau21)
+        t.SetBranchAddress("tau32",tau32)
+        t.SetBranchAddress("msd",msd)
+        t.SetBranchAddress("msd",msd)
+        t.SetBranchAddress("deltaphi",deltaphi)
+        t.SetBranchAddress("pt",pt)
+      
+        c = ROOT.TCanvas()
 
-    weightsfile = "weights/TMVAClassification_BDTG.weights.xml"
-    reader.BookMVA("BDTG", weightsfile)
-    
-    nentries = t.GetEntries()
-    
+        hBDT = ROOT.TH1F("BDT", "BDT", 100, -1.0, 1.0)
+        for i in t:    
+          BDT = reader.EvaluateMVA("BDTG")
+          #print "BDT value: ", BDT
+          hBDT.Fill(BDT)
 
-    t.SetBranchAddress("axisminor",axisminor)
-    t.SetBranchAddress("girth",girth)
-    t.SetBranchAddress("tau21",tau21)
-    t.SetBranchAddress("tau32",tau32)
-    t.SetBranchAddress("msd",msd)
-    t.SetBranchAddress("msd",msd)
-    t.SetBranchAddress("deltaphi",deltaphi)
-    t.SetBranchAddress("pt",pt)
+
+        hBDT.Draw()
+        if(not os.path.isdir("plots")): os.system('mkdir plots')
+        c.Print("plots"+label+"_BDT.pdf")
+        return hBDT
+
+
+
+def getBDT(path, sample):
+    filename = path + "tree_" + sample.label + ".root"
+    print "FILENAME: ",filename 
+    hBDT = getSingleBDT(filename, sample.label)
+    return hBDT
+
     
-    c = ROOT.TCanvas()
+def getCompositeBDT(path, sample):
 
     hBDT = ROOT.TH1F("BDT", "BDT", 100, -1.0, 1.0)
-    for i in t:    
-        BDT = reader.EvaluateMVA("BDTG")
-        #print "BDT value: ", BDT
-        hBDT.Fill(BDT)
 
+    for s in sample.components:
+        h = getBDT(path, s)
+        sf = s.sigma/h.Integral()
+        hBDT.Add(h, sf)
 
-    hBDT.Draw()
     return hBDT
-    c.Print(filename+ "_BDT.pdf")
 
-
+    
 def getEffs(hBDT, step):
     min = hBDT.GetXaxis().GetBinLowEdge(hBDT.GetXaxis().GetFirst())
     print "First ", min
@@ -103,7 +128,7 @@ def getEffs(hBDT, step):
         print "Eff: ", eff
         y.append(1. - eff)
 
-    return x,y
+    return y
 
 
 def getROC(effSig, rejBkg):
@@ -116,10 +141,23 @@ def getROC(effSig, rejBkg):
     graph = ROOT.TGraph(len(x), x, y )
     c.cd()
     graph.Draw("AP")
-    c.Print("graph.pdf")
+    if(not os.path.isdir("plots")): os.system('mkdir plots')
+    c.Print("plots/graph.pdf")
 
-    
-h = getBDT("tree_QCD_Pt_470to600.root")
-x, effBkg = getEffs(h, 0.01)
+
+
+
+
+path = "/mnt/t3nfs01/data01/shome/grauco/SVJAna/CMSSW_8_0_20/src/BDT/Ntuple/"            
+#h = getBDT(path, QCD_Pt_470to600)
+h = getBDT(path,SVJ_mZ3000_mDM1_rinv03_alpha02)
+effBkg = getEffs(h, 0.01)
 getROC(effBkg, effBkg)
+
+
+
+
+
+
+
 
