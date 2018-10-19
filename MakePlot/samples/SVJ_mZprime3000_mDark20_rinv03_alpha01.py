44ddc2a199f96152fd6ea3d22eebd62824291473
grauco <giorgia.rauco@cern.ch>
2018-10-19 10:52:06 +0200

    add samples configs for plotting

---
+++ MakePlot/samples/SVJ_mZprime3000_mDark20_rinv03_alpha01.py
@@ -0,0 +1,13 @@
+from utils import *
+
+SVJ_mZprime3000_mDark20_rinv03_alpha01 = sample()
+SVJ_mZprime3000_mDark20_rinv03_alpha01.files = outlist (d,"SVJ_mZprime-3000_mDark-20_rinv-0.3_alpha-0.2")
+SVJ_mZprime3000_mDark20_rinv03_alpha01.skimEff = 1.
+SVJ_mZprime3000_mDark20_rinv03_alpha01.sigma = 1
+SVJ_mZprime3000_mDark20_rinv03_alpha01.jpref = jetLabel
+SVJ_mZprime3000_mDark20_rinv03_alpha01.jp = jetLabel
+SVJ_mZprime3000_mDark20_rinv03_alpha01.color =  ROOT.kRed 
+SVJ_mZprime3000_mDark20_rinv03_alpha01.style = 1
+SVJ_mZprime3000_mDark20_rinv03_alpha01.fill = 0
+SVJ_mZprime3000_mDark20_rinv03_alpha01.leglabel = "#font[50]{m}_{#font[40]{Z'}}=3000, #font[50]{m}_{#font[40]{Dark}}=20, #font[50]{r}_{#font[40]{inv}}=0.3, #font[50]{#alpha}=0.2"
+SVJ_mZprime3000_mDark20_rinv03_alpha01.label = "SVJ_mZprime3000_mDark20_rinv03_alpha01"
---
+++ MakePlot/samples/utils.py
@@ -0,0 +1,22 @@
+
+import ROOT
+import collections
+import os, commands
+
+
+d = "/scratch/decosa/v9/"
+
+jetLabel = "jetsAK4_"
+
+
+class sample(object):
+    pass
+
+
+def outlist(repodir,dirn):
+    cmd = "ls " + repodir + dirn
+    status,ls_la = commands.getstatusoutput( cmd )
+    list = ls_la.split(os.linesep)
+    files = [d + "/"+dirn+ "/"+ l for l in list]
+    return files
+
