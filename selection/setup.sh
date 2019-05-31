8fc344636e273ba5cd0e9bc191f67e9991af2388
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-05-31 15:27:51 +0200

    should have fixed setup file for selection

--- selection/setup.sh
+++ selection/setup.sh
@@ -1,3 +1,4 @@
+cmsenv
 alias svj="python $(pwd)/SVJselection.py $@"
 alias svjtest="python $(pwd)/SVJselection.py $@ -i $(pwd)/intest -o $(pwd)/outtest -d -t -c -f qcd*"
 echo "selection aliases setup"
