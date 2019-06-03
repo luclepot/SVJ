d1def71e71b700a7e57317bd07ca6b02ff91504b
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-06-03 16:49:14 +0200

    updating condor use instructions

--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentError
 from glob import glob
 
 BASE_COMMAND = 'env -i HOME=$HOME bash -i -c "<CMD>"'
@@ -22,12 +22,11 @@ def logbase(s, prefix):
     else:
         print prefix + str(line)
 
-
 def _range_input(s):
     try:
         return tuple(map(int, s.strip().strip(')').strip('(').split(',')))
     except:
-        raise argparse.ArgumentTypeError("-r input not in format: int,int")
+        raise ArgumentError("-r input not in format: int,int")
 
 def _smartpath(s):
     if s.startswith('~'):
@@ -49,6 +48,45 @@ def _check_for_default_file(pathoptions, name, pattern, suffix="txt"):
     error("quiting")
     sys.exit(1)
 
+def condor_submit(
+    cmd,
+    outputdir,
+    name,
+):
+    log("writing queue commands")
+    run_script = os.path.join(outputdir, "{0}_submit.sh".format(name))
+    run_submit = os.path.join(outputdir, "{0}_condor.submit".format(name))
+    with open(run_script, 'w+') as f:
+        f.write("#!/bin/bash\n")
+        f.write("echo 'RUNNING NOW'\n")
+        f.write("\n")
+        # f.write(cmd + "\n")
+        for line in cmd.split("; "):
+            f.write("{}\n".format(line))
+    with open(run_submit, 'w+') as f:
+        f.write("executable = {}\n\n".format(run_script))
+        f.write("universe = vanilla\n")
+        f.write("getenv = True\n")
+        f.write("log = {0}\n".format(os.path.join(outputdir, "{0}.log".format(name))))
+        f.write("output = {0}\n".format(os.path.join(outputdir, "{0}.out".format(name))))
+        f.write("error = {0}\n".format(os.path.join(outputdir, "{0}.err".format(name))))
+        f.write("should_transfer_files = YES\n")
+        f.write("when_to_transfer_output = ON_EXIT_OR_EVICT\n")
+        # f.write("transfer_output_files = Data\n")
+        # f.write("request_cpus = 1\n")
+        # f.write("request_disk = 20MB\n")
+        # f.write("request_memory = 20MB\n\n")
+        f.write("queue\n")
+    os.system("chmod +rwx {}".format(run_script))
+    os.system("condor_submit {}; condor_q".format(run_submit))
+    sys.exit(0)
+
+def local_submit(
+    master_command,
+):
+    os.system(master_command)
+    sys.exit(0)
+
 ### parser setup
 
 def setup_parser():
@@ -65,6 +103,7 @@ def setup_parser():
         subparser.add_argument('-i', '--input', dest="inputdir", action="store", type=_smartpath, help="input dir path", required=True)
         subparser.add_argument('-o', '--output', dest="outputdir", action="store", type=_smartpath, help="output dir path", required=True)
         subparser.add_argument('-n', '--name', dest='name', action='store', default='sample', help='sample save name')
+        subparser.add_argument('-j', '--batch-job', dest='batch', action='store', default=None, help='attempt to run as a batch job on the indicated service')
     
     # selection args
     select.add_argument('-f', '--filter', dest='filter', action='store', default='*', help='glob-style filter for root files in inputfile')
@@ -83,7 +122,7 @@ def setup_parser():
 
     return parser
 
-def select_main(inputdir, outputdir, name, filter, range, debug, timing, cuts, build, dryrun, gdb):
+def select_main(inputdir, outputdir, name, batch, filter, range, debug, timing, cuts, build, dryrun, gdb):
     log("running command 'select'")
     
     inputdir = _smartpath(inputdir)
@@ -98,7 +137,7 @@ def select_main(inputdir, outputdir, name, filter, range, debug, timing, cuts, b
     # get list of samples, write to text file
     criteria = os.path.join(inputdir, ffilter)
     samplenames = glob(criteria)
-    samplefile = os.path.join(outputdir, "{0}_filelist.txt".format(args.name))
+    samplefile = os.path.join(outputdir, "{0}_filelist.txt".format(name))
 
     if len(samplenames) == 0:
         error("No samples found matching glob crieria '{0}'".format(criteria))
@@ -114,25 +153,48 @@ def select_main(inputdir, outputdir, name, filter, range, debug, timing, cuts, b
             sf.write(samplename + '\n')
 
     path = os.path.abspath(os.path.dirname(__file__))
-    setup_command = "source selection/setup.sh"
+    setup_command = "source {0}".format(os.path.join(path, "selection/setup.sh"))
+
     if build:
         setup_command += "; cd {0}; cd ../..; scram b -j 10; cd {1}".format(path, path)
         
-    run_command = 'SVJselection ' + ' '.join([samplefile, name, outputdir] + list(map(lambda x: str(int(x)), [debug, timing, cuts, rng[0], rng[1]])))
-    os.system(BASE_COMMAND.replace("<CMD>", setup_command + "; " + run_command))
-    sys.exit(0)
+    run_command = 'cd {0}; ../../bin/sl*/SVJselection '.format(path) + ' '.join([samplefile, name, outputdir] + list(map(lambda x: str(int(x)), [debug, timing, cuts, rng[0], rng[1]])))
+    master_command = setup_command + "; " + run_command
 
-def convert_main(inputdir, outputdir, name, range, DR, NC):
+    if dryrun:
+        log("DRYRUN: command is:")
+        log(master_command)
+        sys.exit(0)
+    
+    if batch is not None:
+        if batch == "condor":
+            condor_submit(master_command, outputdir, name)
+        else:
+            raise ArgumentError("unrecognized batch platform '{0}'".format(batch))
+
+    master_command = BASE_COMMAND.replace("<CMD>", master_command)
+
+    local_submit(master_command)
+
+def convert_main(inputdir, outputdir, name, batch, range, DR, NC):
     log("running command 'convert'")
     filespec = _check_for_default_file([inputdir, outputdir], name, 'filelist')
     spath = _check_for_default_file([inputdir, outputdir], name, 'selection')
     setup_command = "source conversion/setup.sh"
     python_command = "python conversion/h5converter.py "
     python_command += " ".join(map(str, [inputdir, outputdir, filespec, spath, name, DR, NC, range[0], range[1]]))
-    os.system(BASE_COMMAND.replace("<CMD>", "; ".join([setup_command, python_command])))
+
+    master_command = BASE_COMMAND.replace("<CMD>", "; ".join([setup_command, python_command]))
+
+    if dryrun:
+        log("DRYRUN: command is:")
+        log(master_command)
+        sys.exit(0)
+        
+    os.system(master_command)
     sys.exit(0)
 
-def train_main(inputdir, outputdir, name, filter):
+def train_main(inputdir, outputdir, name, batch, filter):
     log("running command 'train'")
     sys.exit(0)
 
