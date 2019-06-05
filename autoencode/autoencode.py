fdb2b5864e9eaf6d2ad59a5c31f4f8199031457a
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-06-05 14:02:10 +0200

    add vizualization etc. to autoencoding

--- autoencode/autoencode.py
+++ autoencode/autoencode.py
@@ -2,6 +2,8 @@ import numpy as np
 from keras.layers import Input, Dense
 from keras.models import Model
 import keras.optimizers
+import keras.initializers
+import keras.callbacks
 from collections import OrderedDict as odict
 import os
 from operator import mul
@@ -25,6 +27,8 @@ class basic_autoencoder:
         self.processed = odict()
         self.data = odict()
         self.labels = odict()
+        self.train_params = odict()
+        self.build_params = odict()
 
     def add_sample(
         self,
@@ -97,17 +101,23 @@ class basic_autoencoder:
 
         self.BUILT_TRAINING_DATASET = True
 
-    # def reconstruct_output(
-    #     self,
-    #     output_dataset,
-    # ):
-
+    def reconstruct_output(
+        self,
+        output_dataset,
+    ):
+        
 
     def build(
         self,
         bottleneck_dim,
         intermediate_dims=[],
         loss_function="mse",
+        kinit="random_uniform",
+        binit="random_uniform",
+        reg=keras.regularizers.l1(10e-8),
+        intermediate_activation='relu',
+        encoded_activation='relu',
+        output_activation='tanh',
     ):
 
         self.build_training_dataset()
@@ -122,13 +132,13 @@ class basic_autoencoder:
 
         if len(intermediate_dims) > 0:
             interms1 = []
-            interms1.append(Dense(self.intermediate_dims[0], activation='relu')(self.inputs))
+            interms1.append(Dense(self.intermediate_dims[0], activation=intermediate_activation, kernel_initializer=kinit, bias_initializer=binit, activity_regularizer=reg)(self.inputs))
             for i,dim in enumerate(self.intermediate_dims[1:]):
-                interms1.append(Dense(dim, activation='relu')(interms1[i - 1]))
+                interms1.append(Dense(dim, activation=intermediate_activation, kernel_initializer=kinit, bias_initializer=binit, activity_regularizer=reg)(interms1[i - 1]))
         else:
             interms1 = [self.inputs]
 
-        encoded = Dense(self.bottleneck_dim, activation='relu')(interms1[-1])
+        encoded = Dense(self.bottleneck_dim, activation=encoded_activation, kernel_initializer=kinit, bias_initializer=binit, activity_regularizer=reg)(interms1[-1])
 
         self.encoder = Model(self.inputs, encoded, name='encoder')
         self.encoder.summary()
@@ -137,13 +147,13 @@ class basic_autoencoder:
 
         if len(self.intermediate_dims) > 0:
             interms2 = []
-            interms2.append(Dense(self.intermediate_dims[0], activation='relu')(decode_inputs))
+            interms2.append(Dense(self.intermediate_dims[0], activation=intermediate_activation, kernel_initializer=kinit, bias_initializer=binit, activity_regularizer=reg)(input_img)(decode_inputs))
             for i,dim in enumerate(intermediate_dims[1:]):
-                interms2.append(Dense(dim, activation='relu')(interms2[i - 1]))
+                interms2.append(Dense(dim, activation=intermediate_activation, kernel_initializer=kinit, bias_initializer=binit, activity_regularizer=reg)(interms2[i - 1]))
         else:
             interms2 = [decode_inputs]
 
-        self.outputs = Dense(self.output_dim, activation='tanh')(interms2[-1])
+        self.outputs = Dense(self.output_dim, activation=output_activation)(interms2[-1])
 
         self.decoder = Model(decode_inputs, self.outputs, name='decoder')
         self.decoder.summary()
@@ -156,6 +166,16 @@ class basic_autoencoder:
         # self.autoencoder.add_loss(self.loss)
         self.loss = loss_function
         self.autoencoder.summary()
+
+        
+        self.build_params['kinit'] = kinit
+        self.build_params['binit'] = binit
+        self.build_params['n_samples'] = self.n_samples
+        self.build_params['intermediate_activation'] = intermediate_activation
+        self.build_params['encoded_activation'] = encoded_activation
+        self.build_params['output_activation'] = output_activation
+        self.build_params['loss'] = self.loss
+        self.build_params['shape'] = tuple([self.input_dim,] + self.intermediate_dims + [self.bottleneck_dim,] + list(np.flip(self.intermediate_dims,0)) + [self.output_dim])
 
         self.BUILT = True
 
@@ -173,7 +193,9 @@ class basic_autoencoder:
     ):
         assert self.BUILT
 
-        self.autoencoder.compile(getattr(keras.optimizers, optimizer)(lr=learning_rate), 'mse')
+        if optimizer in ['adagrad', 'adadelta']:
+            learning_rate = 1.0
+        self.autoencoder.compile(getattr(keras.optimizers, optimizer)(lr=learning_rate), self.loss)
         self.history = self.autoencoder.fit(
             self.normalized,
             self.normalized,
@@ -183,11 +205,15 @@ class basic_autoencoder:
             validation_split=validation_split,
             steps_per_epoch=int(np.ceil(self.n_samples*(1-validation_split)/batch_size)),
             validation_steps=int(np.ceil(self.n_samples*validation_split/batch_size)),
-            shuffle=shuffle,
+            shuffle=shuffle
             )
 
         self.reps = self.encoder.predict(self.normalized)
-        self.autoencoder.save_weights(os.path.join(self.path, "{0}_weights.h5".format(self.name)))
+        self.train_params['validation_split'] = validation_split
+        self.train_params['epochs'] = epochs
+        self.train_params['batch_size'] = batch_size
+        self.train_params['optimizer'] = optimizer
+        self.train_params['learning_rate'] = learning_rate
         self.TRAINED = True
 
     def plot_training_history(
@@ -200,16 +226,38 @@ class basic_autoencoder:
     def plot_rep(
         self,
         i,
+        *args,
+        **kwargs
     ):
         assert i < self.normalized.shape[0]
+        n = np.sqrt(self.normalized.shape[1])
+        assert np.ciel(n) == np.floor(n) ## assert IS SQUARE (lol)
+        plt.pcolormesh(self.reps[i].reshape(n,n), *args, **kwargs)
+        plt.colorbar()
+        plt.show()        
+
+    def plot_rep_distributions(
+        self,
+        hide_zeros=False
+    ):
+        for i,dist in enumerate(self.reps.T):
+            if hide_zeros and all(np.isclose(dist,0)):
+                continue
+            plt.hist(dist, range=(self.reps.min(), self.reps.max()), bins=40, histtype='step', label="param " + str(i))
+        plt.legend()
+        plt.show()
         
     def load(
         self,
         filename=None,
     ):
         assert self.BUILT
-        base_filename = filename or os.path.join(self.path, self.name + "_weights.h5")
-        raise NotImplementedError
+        base_filename = self.base_fname()
+        if os.path.exists(base_filename):
+            self.autoencoder.load_weights(base_filename)
+            return True
+        print "no weights found with filename " + base_filename
+        return False
 
     def save(
         self, 
@@ -217,8 +265,17 @@ class basic_autoencoder:
     ):
         assert self.BUILT
         assert self.TRAINED
-        base_filename = filename or os.path.join(self.path, self.name + "_weights.h5")
-        raise NotImplementedError
+        base_filename = filename or self.base_fname()
+        if not os.path.exists(base_filename):
+            self.autoencoder.save_weights(base_filename)
+            return True
+        print "error: identically named file already exists at " + base_filename
+        return False
+        
+    def base_fname(
+        self,
+    ):
+        return os.path.join(self.path, self.name + "_weights.h5")
 
     def _smartpath(
         self,
@@ -228,17 +285,23 @@ class basic_autoencoder:
             return path
         return os.path.abspath(path)
 
-
 if __name__=="__main__":
     samplepath = "../data/first10/first10_data.h5"
     samplepath = "../data/full/0_data.h5"
-    b = basic_autoencoder("testsample", path="")
-    b.add_sample(samplepath)
+    b = basic_autoencoder("large", path="")
+    b.add_sample("../data/full/0_data.h5")
+    b.add_sample("../data/full/2_data.h5")
+    b.add_sample("../data/full/5_data.h5")
+    b.add_sample("../data/full/6_data.h5")
+    b.add_sample("../data/full/7_data.h5")
+    b.add_sample("../data/full/8_data.h5")
+
     # b.add_sample("../data/output/smallsample_data.h5")
     b.process_samples()
-    b.build(9, [200,50], loss_function="mean_absolute_error")
-    b.train(epochs=25, batch_size=20, validation_split=0.25, learning_rate=0.01)
-    b.plot_training_history()
+    b.build(9, loss_function="binary_crossentropy")
+    b.train(epochs=100, batch_size=50, validation_split=0.3, learning_rate=0.01, optimizer="adadelta")
+    # b.plot_training_history()
+    b.plot_rep_distributions(hide_zeros=False)
     # print ret.values()[1].shape
 
 # def autoencode(
