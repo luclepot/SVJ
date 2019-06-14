7e0a5fc0a735b5c8682fab95bdcaf9a4973b8f21
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-06-14 18:07:39 +0200

    move vanilla to models, removes lots of componentes from autoencode. - autoencode.py should become a driver file with several programs

--- autoencode/models.py
+++ autoencode/models.py
@@ -1,6 +1,4 @@
 
-
-
 def basic_run(bn, optimizer, loss, act, epochs, batch):
     b = data_loader(True)
     b.add_sample("../data/hlfSVJ/full.h5")
@@ -49,3 +47,52 @@ def basic_run(bn, optimizer, loss, act, epochs, batch):
         "true": x_norm_train.tolist(),
         "pred": autoencoder.predict(x_norm_train).tolist(),
     }
+
+def rand_search(n):
+    def r(l):
+        return l[random.randint(0, len(l) - 1)]
+    optimizers = ['adam', 'adagrad']
+    losses = ['mean_squared_error','mean_absolute_error']
+    activations = ['linear', 'tanh']
+    bottlenecks = [4,5,6,7,8]
+    batches = [10,50,100]
+    epochs = [40]
+    pdict = odict()
+    
+    import json
+
+    for i in range(n):
+
+        opt = r(optimizers)
+        loss = r(losses)
+        act = r(activations)
+        bn = r(bottlenecks)
+        batch = r(batches)
+        epoch = r(epochs)
+
+        strname = "{}-{}-{}-{}-{}-{}".format(bn, opt, loss, act, batch, epoch)
+
+        if strname in pdict:
+            continue
+
+        pdict[strname] = {}
+
+        pdict[strname]['params'] = {
+            'optimizer': opt,
+            'loss': loss,
+            'activation': act,
+            'bottleneck': bn,
+            'epochs': epoch,
+            'batchsize': batch,
+        }
+
+        print strname
+
+        pdict[strname]['out'] = basic_run(bn, opt, loss, act, epoch, batch)
+
+        if os.path.exists("pdict.json"):
+            old = json.load(open("pdict.json"))
+            pdict.update(old)
+        json.dump(pdict, open("pdict.json", 'w+'))
+
+        print pdict.keys()
