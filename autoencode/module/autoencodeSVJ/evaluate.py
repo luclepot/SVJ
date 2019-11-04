8eba846a80ca29cdfcc787909a769afa3be80654
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-11-05 00:26:10 +0100

    fixing some pieces of evaluate; adding a new object for data loading

--- autoencode/module/autoencodeSVJ/evaluate.py
+++ autoencode/module/autoencodeSVJ/evaluate.py
@@ -852,4 +852,99 @@ def ae_train(
     #     hlf_to_drop=['Energy', 'Flavor'],
     # ):
         
+class signal_element(object):
+    def __init__(
+        self,
+        path,
+        name
+    ):
+        self._hlf = None
+        self._eflow = None
+        self._hlf_to_drop = []
+        self._name = name
+        self._path = path
+        self._loaded = False
+
+    def _load(
+        self,
+        hlf=True,
+        eflow=True,
+        hlf_to_drop=['Energy', 'Flavor'],
+    ):
+        if all([hlf == self._hlf, self._eflow == eflow, set(self._hlf_to_drop) == set(hlf_to_drop)]) and self._loaded:
+            return 
+
+        self._hlf = hlf
+        self._eflow = eflow
+        self._hlf_to_drop = hlf_to_drop
+        self._loaded = True
+        
+        (self.data,
+         self.jets,
+         self.event,
+         self.flavor) = utils.load_all_data(
+            self._path, self._name, 
+            include_hlf=self._hlf, include_eflow=self._eflow, hlf_to_drop=self._hlf_to_drop
+        )
+
+    def _add_attribute(
+        self,
+        name,
+        function,
+    ):
+        setattr(self, name, function(self))
+        
+    def _rm_attribute(
+        self,
+        name
+    ):
+        delattr(self, name)
+
+class data_holder(object):
+    def __init__(
+        self,
+        **kwargs
+    ):
+        self.KEYS = {}
+        names = sorted(kwargs.keys())
+        for name in names:
+            path = kwargs[name]
+            if name[0].isdigit():
+                name = 'Zprime_' + name
+            name = name.replace('.', '')
+            # print('loading {} from path \'{}\'...'.format(name, path))
+            setattr(self, name, signal_element(path, name))
+            self.KEYS[name] = getattr(self, name)
+
+        print('found {} datasets'.format(len(names)))
+
+    def load(
+        self,
+        hlf=True,
+        eflow=True,
+        hlf_to_drop=['Energy', 'Flavor']
+    ):
+        for k,v in self.KEYS.items():
+            v._load(hlf, eflow, hlf_to_drop)
+
+    def add_attribute(
+        self,
+        name,
+        function,
+    ):
+        for k,v in self.KEYS.items():
+            v._add_attribute(name, function)
+    
+    def rm_attribute(
+        self,
+        name
+    ):
+        for k,v in self.KEYS.items():
+            v._rm_attribute(name)
+
+    def get(
+        self,
+        name
+    ):
+        return [getattr(v, name) for v in self.KEYS.values()]
 
